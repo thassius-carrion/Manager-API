26d3f2190ecdae8a2d25ed913ea890ddc9644352
Thassius <thassius.carrion@hotmail.com>
2022-04-27 15:44:21 -0300

    Project created, Started entity layer

---
+++ src/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,62 @@
+using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators {
+
+    public class UserValidator : AbstractValidator<User>{
+
+        public UserValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("Entity can not be empty.")
+
+                .NotNull()
+                .WithMessage("Entity can not be null.");
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("Name can not be null.")
+
+                .NotEmpty()
+                .WithMessage("Name can not be empty.")
+
+                .MinimumLength(3)
+                .WithMessage("Name must have at least 3 characters.")
+
+                .MaximumLength(70)
+                .WithMessage("Name must have maximum of 70 characters.");
+            
+            RuleFor(x => x.Email)
+                .NotNull()
+                .WithMessage("Email can not be null.")
+
+                .NotEmpty()
+                .WithMessage("Email can not be empty.")
+
+                .MinimumLength(10)
+                .WithMessage("Email must have at least 10 characters.")
+
+                .MaximumLength(180)
+                .WithMessage("Email must have maximum of 180 characters.")
+
+                .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+                .WithMessage("Email invalid.");
+            
+            RuleFor(x => x.Password)
+                .NotNull()
+                .WithMessage("Password can not be null.")
+
+                .NotEmpty()
+                .WithMessage("Password can not be empty.")
+
+                .MinimumLength(6)
+                .WithMessage("Password must have at least 6 characters.")
+
+                .MaximumLength(30)
+                .WithMessage("Password must have maximum of 30 characters.");
+             
+        }
+
+    }
+}
